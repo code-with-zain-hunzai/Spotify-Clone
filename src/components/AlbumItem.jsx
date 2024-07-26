bcb2f22d032ba514788275222278b5e323cbecba
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-26 15:33:48 +0500

    work on ui

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AlbumItem = ({ image, name, desc, id }) => {
+    return (
+        <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+            <img className='rounded' src={image} alt="" />
+            <p className='font-bold mt-2 mb-1'>{name}</p>
+        </div>
+    )
+}
+
+export default AlbumItem
