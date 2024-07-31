616750117f7f14e175bc5b26eed9415bdb8cbcd3
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-31 14:01:16 +0500

    Add all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
-import Display from './components/Display'
+import React, { useContext } from 'react';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import Display from './components/Display';
+import { playerContext } from './context/PlayerContext.jsx';
 
 const App = () => {
+  const { audioRef, track } = useContext(playerContext);
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
-        <Display/>
+        <Display />
       </div>
       <Player />
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import DisplayHome from './DisplayHome'
+import React, { useRef, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import DisplayHome from './DisplayHome';
+import DisplayAblum from './DisplayAblum';
+import { albumsData } from '../assets/assets';
 
 const Display = () => {
+    const displayRef = useRef();
+    const location = useLocation();
+
+    const isAlbum = location.pathname.includes("album");
+    const albumId = isAlbum ? location.pathname.split('/').pop() : '';
+    const bgColor = albumsData[Number(albumId)]?.bgColor || '#121212';
+
+    useEffect(() => {
+        if (albumId) {
+            displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+        } else {
+            displayRef.current.style.background = `#121212`;
+        }
+    }, [albumId, bgColor]);
+
     return (
-        <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+        <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
             <Routes>
                 <Route path='/' element={<DisplayHome />} />
+                <Route path='/album/:id' element={<DisplayAblum />} />
             </Routes>
         </div>
-    )
+    );
 }
 
-export default Display
+export default Display;
