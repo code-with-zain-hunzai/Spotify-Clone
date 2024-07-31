ed9d15eb752a4bc0bb4b392885c6ee7d0b031744
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-31 20:33:34 +0500

    fix player issue

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const playerContext = createContext();
@@ -41,6 +41,25 @@ const PlayerContextProvider = (props) => {
         time, setTime,
         play,pause
     }
+
+    useEffect(() => {
+      setTimeout(()=>{
+        audioRef.current.ontimeupdate=()=>{
+            seekBar.current.style.width=(Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%"
+            setTime({
+                currentTime: {
+                    second: Math.floor(audioRef.current.currentTime % 60),
+                    minute: Math.floor(audioRef.current.currentTime / 60)
+                },
+                totalTime: {
+                    second: Math.floor(audioRef.current.duration % 60),
+                    minute: Math.floor(audioRef.current.duration / 60)
+                }
+            })
+        }
+      },1000)
+    }, [audioRef])
+    
 
     return (
         <playerContext.Provider value={contextValue}>
