2439d29e8a102675ccaf929b7e1737e9e4782293
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-31 19:51:35 +0500

    Resolve merge conflicts and update code

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
+
 import React, { useRef, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import DisplayHome from './DisplayHome';
 import DisplayAblum from './DisplayAblum';
 import { albumsData } from '../assets/assets';
-=======
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import DisplayHome from './DisplayHome'
-import DisplayAlbum from './DisplayAlbum'
->>>>>>> 946fe6accdd326ba402c9f0fd7900a216f05482b
 
 const Display = () => {
     const displayRef = useRef();
