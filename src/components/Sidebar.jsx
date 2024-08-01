a57185b1fdb33ede2aae5ea7e4937ef5d80f422b
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-08-01 13:08:34 +0500

    chnages and update code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 const Sidebar = () => {
+
+    const navigate = useNavigate();
     return (
         <div className='w-[25%] flex-col p-2 h-full gap-2 text-white hidden lg:flex'>
             <div className="flex flex-col bg-[#121212] h-[15%] rounded justify-around">
-                <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <div onClick={() => navigate('/')} className="flex items-center gap-3 pl-8 cursor-pointer">
                     <img className='w-6' src={assets.home_icon} alt="" />
                     <p className=''>Home</p>
                 </div>
--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -31,6 +31,31 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false)
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true)
+    }
+
+    const previous = async () => {
+        if (track.id > 0) {
+            await setTrack(songsData[track.id - 1])
+            await audioRef.current.play()
+            setPlayStatus(true)
+        }
+    }
+    const next = async () => {
+        if (track.id < songsData.length - 1) {
+            await setTrack(songsData[track.id + 1])
+            await audioRef.current.play()
+            setPlayStatus(true)
+        }
+    }
+
+    const seekSong = async (e)=>{
+audioRef.current.currentTime=((e.nativeEvent.offsetX/seekBg.current.offsetWidth)*audioRef.current.duration)
+    }
+
     const contextValue = {
         audioRef,
         seekBar,
@@ -39,27 +64,30 @@ const PlayerContextProvider = (props) => {
         setTrack,
         playStatus, setPlayStatus,
         time, setTime,
-        play,pause
+        play, pause,
+        playWithId,
+        previous, next,
+        seekSong
     }
 
     useEffect(() => {
-      setTimeout(()=>{
-        audioRef.current.ontimeupdate=()=>{
-            seekBar.current.style.width=(Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%"
-            setTime({
-                currentTime: {
-                    second: Math.floor(audioRef.current.currentTime % 60),
-                    minute: Math.floor(audioRef.current.currentTime / 60)
-                },
-                totalTime: {
-                    second: Math.floor(audioRef.current.duration % 60),
-                    minute: Math.floor(audioRef.current.duration / 60)
-                }
-            })
-        }
-      },1000)
+        setTimeout(() => {
+            audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100)) + "%"
+                setTime({
+                    currentTime: {
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        minute: Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime: {
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60)
+                    }
+                })
+            }
+        }, 1000)
     }, [audioRef])
-    
+
 
     return (
         <playerContext.Provider value={contextValue}>
